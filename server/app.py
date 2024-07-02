cdcb7003ad5e664793c4db41eceae516a38e4b4c
whitney-shisia <shisiawhitney215@gmail.com>
2024-07-02 08:29:58 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -47,11 +47,34 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
-
+class Login(Resource):
+      def post(self):
+          user = User.query.filter(User.username==request.json['username']
+                ).first()
+          session["user_id"] = user.id
+          response = user.to_dict()
+          
+          return make_response(response, 200)
+    
+class Logout(Resource):
+     def delete(self):
+         session['user_id'] = None
+         return {'message': 'Logged out'}, 204
+     
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        else:
+           return {}, 401     
+        
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
